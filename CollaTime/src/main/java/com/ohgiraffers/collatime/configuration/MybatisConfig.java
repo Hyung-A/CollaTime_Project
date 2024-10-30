9e7c6084e7146821b8a7fe80b96bcdc4fe1340d9
JUHYE0925 <rhkdlf48301@gmail.com>
2024-10-30 09:36:24 +0900

    241030

--- CollaTime/src/main/java/com/ohgiraffers/collatime/configuration/MybatisConfig.java
+++ CollaTime/src/main/java/com/ohgiraffers/collatime/configuration/MybatisConfig.java
@@ -1,13 +1,10 @@
 package com.ohgiraffers.collatime.configuration;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(basePackages = "com.ohgiraffers.collatime", annotationClass = Mapper.class)
-public class MybatisConfig {
-
-
-
+@MapperScan(basePackages = "com.ohgiraffers.collatime")
+public class MyBatisConfig {
 }
